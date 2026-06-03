7f8994ef72a1d963a292652b21c149d44b687adc
thiagomktsantos <thiagomktsantos@gmail.com>
2026-06-02 22:27:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10819,13 +10819,23 @@ setTimeout(ajustarAltura, 1200);
 
 // Auto-expand quando só há 1 análise
 (function() {{
-    var cards = document.querySelectorAll('.analise-card');
-    if (cards.length === 1) {{
-        var singleCard = cards[0];
-        var idMatch = singleCard.id.match(/card_(\d+)/);
-        if (idMatch) {{
-            setTimeout(function() {{ toggleCard(parseInt(idMatch[1])); }}, 150);
+    function tryAutoExpand() {{
+        var cards = document.querySelectorAll('.analise-card');
+        if (cards.length === 1) {{
+            var singleCard = cards[0];
+            var idMatch = singleCard.id.match(/card_(\d+)/);
+            if (idMatch) {{
+                toggleCard(parseInt(idMatch[1]));
+            }}
         }}
+    }}
+    
+    if (document.readyState === 'complete') {{
+        setTimeout(tryAutoExpand, 100);
+    }} else {{
+        window.addEventListener('load', function() {{
+            setTimeout(tryAutoExpand, 100);
+        }});
     }}
 }})();
 </script>"""

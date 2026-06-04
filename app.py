11572f3116902477c3e80e238e64632166f27dc5
thiagomktsantos <thiagomktsantos@gmail.com>
2026-06-04 13:48:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3953,13 +3953,13 @@ html, body {{ background:transparent; font-family:'DM Sans',sans-serif; overflow
         [id^="sr_"] li {{ margin:0 0 3px; line-height:1.6; }}
         [id^="sr_"] li::marker {{ color:#00c162; }}
         [id^="sr_"] hr {{ display:none; }}
-        [id^="sr_"] ol {{ margin:5px 0 15px 28px; }}
-        [id^="sr_"] ol li {{ margin:0 0 3px; line-height:1.6; }}
-        [id^="sr_"] ol li::marker {{ color:#24658e; font-weight:600; }}
+        [id^="sr_"] ol {{ margin:5px 0 15px 28px;list-style:none;counter-reset:meu-contador; }}
+        [id^="sr_"] ol li {{ margin:0 0 3px; line-height:1.6;position:relative;padding-left:35px;margin-bottom:15px; }}
+        [id^="sr_"] ol li::before {{ counter-increment:meu-contador;content:counter(meu-contador);position:absolute;left:0;top:0;background-color:#007bff;color:#ffffff;border-radius:50%;width:25px;height:25px;display:flex;align-items:center;justify-content:center;font-size:14px;font-weight:bold; }}
 
-        #smb ol {{ margin:6px 0 14px 24px; }}
-        #smb ol li {{ margin:0 0 4px; line-height:1.65; }}
-        #smb ol li::marker {{ color:#24658e; font-weight:600; }}
+        #smb ol {{ margin:5px 0 15px 28px;list-style:none;counter-reset:meu-contador; }}
+        #smb ol li {{ margin:0 0 3px; line-height:1.6;position:relative;padding-left:35px;margin-bottom:15px; }}
+        #smb ol li::before {{ counter-increment:meu-contador;content:counter(meu-contador);position:absolute;left:0;top:0;background-color:#007bff;color:#ffffff;border-radius:50%;width:25px;height:25px;display:flex;align-items:center;justify-content:center;font-size:14px;font-weight:bold; }}
         /* Modal markdown styles */
         #smb h1,#smb h2,#smb h3 {{
             font-size:16px; font-weight:800; color:#0f1f35;

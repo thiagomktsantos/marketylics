b6519d6dc2cc43ea9c5ad91c4f8b27b86cb6b457
thiagomktsantos <thiagomktsantos@gmail.com>
2026-07-17 14:15:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8481,7 +8481,7 @@ body {{ padding-bottom:8px; }}
 }}
 .col-title-redes  {{ background:#fff; color:#1d4ed8; border-left:3px solid #3b82f6; }}
 .col-title-site   {{ background:#fff; color:#15803d; border-left:3px solid #22c55e; }}
-.col-title-ads    {{ background:#fff; color:#c2410c; border-left:3px solid #f97316; }}
+.col-title-ads    {{ background:#fff; color:#8b5cf6; border-left:3px solid #9a71f7; }}
 .col-redes  {{ border-left:2px solid #3b82f620; padding-left:8px; margin-left:-8px; }}
 .col-site   {{ border-left:2px solid #22c55e20; padding-left:8px; margin-left:-8px; }}
 .col-ads    {{ border-left:2px solid #f9731620; padding-left:8px; margin-left:-8px; }}
@@ -8700,7 +8700,7 @@ function buildCards() {{
         buildSeoColumn(d,colSite);
         var colAds=document.createElement('div');
         colAds.className='col col-ads';
-        colAds.innerHTML='<div class="col-title col-title-ads"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="#c2410c" viewBox="0 0 16 16" style="vertical-align:-2px;margin-right:4px;flex-shrink:0;"><path fill-rule="evenodd" d="M8.217 5.243C9.145 3.988 10.171 3 11.483 3 13.96 3 16 6.153 16.001 9.907c0 2.29-.986 3.725-2.757 3.725-1.543 0-2.395-.866-3.924-3.424l-.667-1.123-.118-.197a55 55 0 0 0-.53-.877l-1.178 2.08c-1.673 2.925-2.615 3.541-3.923 3.541C1.086 13.632 0 12.217 0 9.973 0 6.388 1.995 3 4.598 3q.477-.001.924.122c.31.086.611.22.913.407.577.359 1.154.915 1.782 1.714m1.516 2.224q-.378-.615-.727-1.133L9 6.326c.845-1.305 1.543-1.954 2.372-1.954 1.723 0 3.102 2.537 3.102 5.653 0 1.188-.39 1.877-1.195 1.877-.773 0-1.142-.51-2.61-2.87zM4.846 4.756c.725.1 1.385.634 2.34 2.001A212 212 0 0 0 5.551 9.3c-1.357 2.126-1.826 2.603-2.581 2.603-.777 0-1.24-.682-1.24-1.9 0-2.602 1.298-5.264 2.846-5.264q.137 0 .27.018"/></svg>Anúncios</div>'+d.ads_block_html;
+        colAds.innerHTML='<div class="col-title col-title-ads"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="#9a71f7" viewBox="0 0 16 16" style="vertical-align:-2px;margin-right:4px;flex-shrink:0;"><path fill-rule="evenodd" d="M8.217 5.243C9.145 3.988 10.171 3 11.483 3 13.96 3 16 6.153 16.001 9.907c0 2.29-.986 3.725-2.757 3.725-1.543 0-2.395-.866-3.924-3.424l-.667-1.123-.118-.197a55 55 0 0 0-.53-.877l-1.178 2.08c-1.673 2.925-2.615 3.541-3.923 3.541C1.086 13.632 0 12.217 0 9.973 0 6.388 1.995 3 4.598 3q.477-.001.924.122c.31.086.611.22.913.407.577.359 1.154.915 1.782 1.714m1.516 2.224q-.378-.615-.727-1.133L9 6.326c.845-1.305 1.543-1.954 2.372-1.954 1.723 0 3.102 2.537 3.102 5.653 0 1.188-.39 1.877-1.195 1.877-.773 0-1.142-.51-2.61-2.87zM4.846 4.756c.725.1 1.385.634 2.34 2.001A212 212 0 0 0 5.551 9.3c-1.357 2.126-1.826 2.603-2.581 2.603-.777 0-1.24-.682-1.24-1.9 0-2.602 1.298-5.264 2.846-5.264q.137 0 .27.018"/></svg>Anúncios</div>'+d.ads_block_html;
         cols.appendChild(colRedes);
         cols.appendChild(colSite);
         cols.appendChild(colAds);
